97595e3380744945bd9b230e890f4dc83de2cf61
Marianna Coppa <densimetra@gmail.com>
2025-04-17 14:33:51 +0200

    Done lesson 179: revalidating data to avoid caching problems on like button

--- actions/posts.js
+++ actions/posts.js
@@ -2,6 +2,7 @@
 import {storePost, updatePostLikeStatus} from "@/lib/posts";
 import {redirect} from "next/navigation";
 import {uploadImage} from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 
 export async function createPost(prevState, formData) {
     const title = formData.get('title');
@@ -46,5 +47,6 @@ export async function createPost(prevState, formData) {
 }
 
 export async function togglePostLikeStatus(postId) {
-    updatePostLikeStatus(postId, 2);
+   await updatePostLikeStatus(postId, 2);
+   revalidatePath('/', 'layout');
 }
